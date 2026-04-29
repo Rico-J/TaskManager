08a9ac0718510af07572b7ea5cea5cf5c5b984e2
Joel Rico <joel.m.rico90@gmail.com>
2026-04-29 06:47:34 -0700

    update1

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -7,6 +7,33 @@ public class TaskManager {
     static Scanner scnr = new Scanner(System.in);
 
     public static void main(String[] args) {
+        while (true) {
+            System.out.println("\n---Task Manager---");
+            System.out.println("1. Show Tasks");
+            System.out.println("2. Add Tasks");
+            System.out.println("3. Delete Tasks");
+            System.out.println("4. Quit");
+
+            System.out.println("Choose option: ");
+            String choice = scnr.nextLine();
+
+            switch (choice){
+                case "1":
+                    showTasks();
+                    break;
+                case "2":
+                    addTask();
+                    break;
+                case "3":
+                    deleteTask();
+                    break;
+                case "4":
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    System.out.println("Error, invalide choice.");
+            }
+        }
         
     }
 }
