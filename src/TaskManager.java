1b586d33e91b1c62fc6394b4b3e3dcf3624ebf95
Joel Rico <joel.m.rico90@gmail.com>
2026-04-29 06:19:22 -0700

    First commit

---
+++ src/TaskManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskManager {
+
+    static ArrayList<String> tasks = new ArrayList<>();
+    static Scanner scnr = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
